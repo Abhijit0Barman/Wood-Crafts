ada39e79afe8d2d25102627e1d02124576906493
Abhijit Barman <113384779+Abhijit0Barman@users.noreply.github.com>
2023-11-03 20:07:59 +0530

    day4

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import MainRoutes from "./Routes/MainRoutes"
 import { SingleProduct } from './pages/SingleProduct';
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,8 +3,8 @@ import { DesktopNav } from "../components/ProductNavbar";
 
 export const Homepage = () => {
   return (
-    <div>Homepage
-        <DesktopNav />
+    <div>
+        
     </div>
 
   )
